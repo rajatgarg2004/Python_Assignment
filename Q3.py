e42460f1ef4ccd678c32253e76fb6dc445d0fc97
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-01-07 02:26:38 +0530

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,8 +1,13 @@
-#SID=StudentID
-SID=int(input("SID:- "))
-Name=str(input("Full Name:- "))
-Gender=str(input("Gender(Initial letter only):- "))
-Course=str(input("Course Name:- "))
-CGPA=float(input("CGPA:- "))
-Student= [SID, Name, Gender, Course, Name, CGPA ]
-print(Student)
+#Take input of the Student ID
+SID=int(input("Write your SID:- "))
+#Take input of the name 
+Name=str(input("Write your Full Name:- "))
+#Take input of the Gender
+Gender=str(input("Write your Gender(M/F/U):- "))
+#Take input of of the course pursued
+Course=str(input("Write your Course Name:- "))
+#Take input of the CGPA
+CGPA=float(input("Write your CGPA:- "))
+#StudentID in List format 
+Student= [SID, Name, Gender, Course, CGPA]
+print("Student:- ", Student)
