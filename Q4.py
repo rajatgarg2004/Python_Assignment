c563d1c448e73932703add601bb289438a502fec
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-01-08 13:50:32 +0530

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,9 +1,9 @@
 #Take Input of the marks of the students
-marks_of_student1=int(input("marks of student1:- "))
-marks_of_student2=int(input("marks of student2:- "))
-marks_of_student3=int(input("marks of student3:- "))
-marks_of_student4=int(input("marks of student4:- "))
-marks_of_student5=int(input("marks of student5:- "))
+marks_of_student1=int(input("Marks of student1:- "))
+marks_of_student2=int(input("Marks of student2:- "))
+marks_of_student3=int(input("Marks of student3:- "))
+marks_of_student4=int(input("Marks of student4:- "))
+marks_of_student5=int(input("Marks of student5:- "))
 #x is the list in unsorted order
 x=[marks_of_student1, marks_of_student2, marks_of_student3, marks_of_student4, marks_of_student5]
 #to sort the elements in list we use variable.sort() function
