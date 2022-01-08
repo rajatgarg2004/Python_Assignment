8c6d720d2767ca70c61d883838eaae269dd7622a
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-01-08 13:13:50 +0530

    Update Q5(b).py

--- Q5(b).py
+++ Q5(b).py
@@ -1,7 +1,5 @@
 #Making colour list of the given colors
 colour_list=['Red','Green', 'White', 'Black', 'Pink', 'Yellow']
-#To remove several elememts we use del function
-del colour_list[3:5]            
-#To add an element we use insert function 
-colour_list.insert(3,"Purple")
-print('The updated colour list after removing "Black" and "Pink" and replacing them with "Purple" :-', colour_list)
+#To replace the colours with "Purple" the following can be done
+colour_list[4]=colour_list[3]="Purple"
+print('The updated colour list :-', colour_list)
