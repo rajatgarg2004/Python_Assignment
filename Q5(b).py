6144b3e2f5f55ce665ac6ec7792e98164721775a
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-01-10 16:24:47 +0530

    Delete Q5(b).py

--- Q5(b).py
+++
@@ -1,5 +0,0 @@
-#Making colour list of the given colors
-colour_list=['Red','Green', 'White', 'Black', 'Pink', 'Yellow']
-#Here to replace the colours with "Purple" the following can be done
-colour_list[3]=colour_list[4]="Purple"
-print('The updated colour list :-', colour_list)
