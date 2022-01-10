8bcbe29a663b45791c8b970b6eb928110c1e7f8b
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-01-10 16:40:21 +0530

    Delete Q5(a).py

--- Q5(a).py
+++
@@ -1,5 +0,0 @@
-#Making colour list of the given colors
-colour_list=['Red','Green', 'White', 'Black', 'Pink', 'Yellow']
-#To remove an element we use pop function
-colour_list.pop(3)
-print('The list after removing the 4th element :-', colour_list)
