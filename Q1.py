a5a1bfc8e7cb47d637ea1800124f1cad8e4fa6ce
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-01-10 16:38:51 +0530

    Update and rename Q1.py to Assignment1(Code).py

--- Q1.py
+++
@@ -1,7 +0,0 @@
-#Take input for 3 integers
-num1=int(input("Write your first number:-"))
-num2=int(input("Write your third number:-"))
-num3=int(input("Write your second number:-"))
-#The formula for average of the 3 numbers is
-average=(num1+num2+num3)/3 
-print('The average of the 3 numbers is ', average)
