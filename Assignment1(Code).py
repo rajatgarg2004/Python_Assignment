7d73f68b86f036e4938efd6ea1ec3c3c1558d463
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-01-14 00:31:50 +0530

    Update Assignment1(Code).py

--- Assignment1(Code).py
+++ Assignment1(Code).py
@@ -57,6 +57,7 @@ colour_list=['Red','Green', 'White', 'Black', 'Pink', 'Yellow']
 #To remove an element we use pop function
 colour_list.pop(3)
 print('The list after removing the 4th element :-', colour_list, '\n')
+
 #Q5(b)
 #Making colour list of the given colors
 colour_list=['Red','Green', 'White', 'Black', 'Pink', 'Yellow']
