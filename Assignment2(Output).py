efd11f336807042010841fde120857f1de0afda7
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-01-24 00:27:40 +0530

    Update and rename Assignment1(Output).py to Assignment2(Output).py

---
+++ Assignment2(Output).py
@@ -0,0 +1,32 @@
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+
+Hey,Rajat Garg Here!
+My SID is 21104032
+I am from EE department and my CGPA is 9.9
+
+8
+58
+50
+224
+40
+14
+0
+
+Enter the first number:- 13
+Enter the second number:- 15
+Enter the third number:- 12
+15
+
+Write your sentence or word and I will tell you whether it contains 'name' or not:- 
+My name is Rajat Garg
+Yes
+
+Enter the first side of the triangle:- 2
+Enter the second side of the triangle:- 5
+Enter the third side of the triangle:- 4
+The triangle can be formed
