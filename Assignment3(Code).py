2650222eb64999d439c64292c4c0651c13b6954a
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-02-14 01:39:38 +0530

    Update Assignment3(Code).py

--- Assignment3(Code).py
+++ Assignment3(Code).py
@@ -30,11 +30,10 @@ print("Question 2")
 
 def is_leap(year):
     
-    if year%4==0:
-        leap=True
+    if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+        return True
     else:
-        leap=False
-    return leap
+        return False
 print("To find the next date:- \n")
 #To continue or break loop for the date given
 
