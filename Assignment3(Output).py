69e49a17a5a7755ac74757daebd29593cd12e265
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-02-14 01:40:23 +0530

    Update and rename Assignment1(Output).py to Assignment3(Output).py

---
+++ Assignment3(Output).py
@@ -0,0 +1,105 @@
+Question 1
+Enter the string: onomatapoeia
+The occurence of: 
+('o', 3)
+('n', 1)
+('m', 1)
+('a', 3)
+('t', 1)
+('p', 1)
+('e', 1)
+('i', 1)
+
+--------------------------------------------------------------------------------
+
+Question 2
+To find the next date:- 
+
+Enter the Day: 29
+Enter the Month: 02
+Enter the year: 2004
+The Next Date is :  1/3/2004
+
+--------------------------------------------------------------------------------
+
+Question 3
+Enter the list of numbers(Also add space between 2 numbers): 1 5 3 25 7
+
+[(1, 1), (5, 25), (3, 9), (25, 625), (7, 49)]
+
+--------------------------------------------------------------------------------
+
+Question 4
+Enter the Grade:- 9
+Your Grade is 'A' and Excellent Performace
+
+--------------------------------------------------------------------------------
+
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+--------------------------------------------------------------------------------
+
+Question 6
+Enter the Name of the Student:- Rajat Garg
+Enter the SID of the Student:- 21104032
+Type 'Y' if want enter another Student's Information or Type 'N' if don't want to Enter:- y
+Enter the Name of the Student:- Akshaj Paintola
+Enter the SID of the Student:- 21105030
+Type 'Y' if want enter another Student's Information or Type 'N' if don't want to Enter:- y
+Enter the Name of the Student:- Dhawal Arora
+Enter the SID of the Student:- 21103034
+Type 'Y' if want enter another Student's Information or Type 'N' if don't want to Enter:- n
+{21104032: 'Rajat Garg', 21105030: 'Akshaj Paintola', 21103034: 'Dhawal Arora'}
+{21105030: 'Akshaj Paintola', 21103034: 'Dhawal Arora', 21104032: 'Rajat Garg'}
+{21103034: 'Dhawal Arora', 21104032: 'Rajat Garg', 21105030: 'Akshaj Paintola'}
+Enter the SID to find the name:- 21104032
+Rajat Garg
+
+--------------------------------------------------------------------------------
+
+Question 7
+Enter the number of fibonacci terms you want:- 
+8
+0
+1
+1
+2
+3
+5
+8
+13
+The Average of the numbers is:- 4.125
+
+--------------------------------------------------------------------------------
+
+Question 8
+Set1 =  {1, 2, 3, 4, 5}
+Set2 =  {8, 2, 4, 6}
+Set3 =  {1, 5, 9, 13, 17}
+a.) 
+
+{1, 3, 5, 6, 8} 
+
+b.) 
+
+{3, 6, 8, 9, 13, 17} 
+
+c.) 
+
+{1, 2, 4, 5} 
+
+d.) 
+
+{6, 7, 8, 9, 10} 
+
+e.) 
+
+{10, 7}
+
+--------------------------------------------------------------------------------
