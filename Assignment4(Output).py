49c5b0118d94504a1ce95e38fc9e583d6870e12c
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-02-28 16:49:12 +0530

    Update and rename Assignment1(Output).py to Assignment4(Output).py

---
+++ Assignment4(Output).py
@@ -0,0 +1,105 @@
+Q1 
+
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+--------------------------------------------------------------------------------
+
+
+Q2 
+
+Enter the number of rows for pascals triangle 6
+Using Loops :-
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+Using Recurssion:-
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+
+--------------------------------------------------------------------------------
+
+ Q3 
+
+Enter 2 numbers:- 
+12 5
+a) 
+
+Checking if the quotient and remainder is a callable value or not.
+False
+False
+b) 
+
+All of the values are non-zero.
+
+
+c) 
+
+The filtered numbers that are greater than 4 are : [6, 6, 7, 7, 7, 8, 8] 
+
+d) 
+
+Set: {8, 6, 7}
+
+
+e) 
+
+Immutable set:-  frozenset({8, 6, 7})
+
+
+f) 
+
+Hash value of the max value from the above set:-  8
+
+
+--------------------------------------------------------------------------------
+
+ Q4 
+
+Object created
+The name of the student it Rajat and SID is 21104032.
+Object deleted.
+
+
+--------------------------------------------------------------------------------
+
+ Q5 
+
+The current database is:
+Mehak has a salary of 40000 rupees
+Ashok has a salary of 50000 rupees
+Viren has a salary of 60000 rupees
+a) 
+
+ The updated salary of Mehak is 70000 
+
+b 
+
+Record of Viren deleted
+
+--------------------------------------------------------------------------------
+
+ Q6 
+
+George's word:-Names
+Barbie's word:-Means
+Other possible words are: ['names', 'anmes', 'amnes', 'amens', 'amesn', 'nmaes', 'mnaes', 'manes', 'maens', 'maesn', 'nmeas', 'mneas', 'menas', 'means', 'measn', 'nmesa', 'mnesa', 'mensa', 'mesna', 'mesan', 'naems', 'anems', 'aenms', 'aemns', 'aemsn', 'neams', 'enams', 'eanms', 'eamns', 'eamsn', 'nemas', 'enmas', 'emnas', 'emans', 'emasn', 'nemsa', 'enmsa', 'emnsa', 'emsna', 'emsan', 'naesm', 'anesm', 'aensm', 'aesnm', 'aesmn', 'neasm', 'enasm', 'eansm', 'easnm', 'easmn', 'nesam', 'ensam', 'esnam', 'esanm', 'esamn', 'nesma', 'ensma', 'esnma', 'esmna', 'esman', 'namse', 'anmse', 'amnse', 'amsne', 'amsen', 'nmase', 'mnase', 'manse', 'masne', 'masen', 'nmsae', 'mnsae', 'msnae', 'msane', 'msaen', 'nmsea', 'mnsea', 'msnea', 'msena', 'msean', 'nasme', 'ansme', 'asnme', 'asmne', 'asmen', 'nsame', 'sname', 'sanme', 'samne', 'samen', 'nsmae', 'snmae', 'smnae', 'smane', 'smaen', 'nsmea', 'snmea', 'smnea', 'smena', 'smean', 'nasem', 'ansem', 'asnem', 'asenm', 'asemn', 'nsaem', 'snaem', 'sanem', 'saenm', 'saemn', 'nseam', 'sneam', 'senam', 'seanm', 'seamn', 'nsema', 'snema', 'senma', 'semna', 'seman']
+If Barbie's word is present in the list , then their friendship is real. 
+
+The Friendship is real.
+--------------------------------------------------------------------------------
