07e709a8f04c0f99e2a451be51c5aa689e78cb0e
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-02-28 16:44:00 +0530

    Update Assignment4(Code).py

--- Assignment4(Code).py
+++ Assignment4(Code).py
@@ -1,4 +1,5 @@
 #Q1
+print("Q1 \n")
 #Defining a function with use of recursion
 def TowerOfHanoi(n , Tower1, Tower3, Tower2):
 	if n == 0:
@@ -9,9 +10,10 @@ def TowerOfHanoi(n , Tower1, Tower3, Tower2):
 n = 3
 TowerOfHanoi(n, 'A', 'C', 'B')
 print("\n")
-
+print("-"*80)
+print("\n")
 #Q2
-
+print("Q2 \n")
 from tracemalloc import start
 def fact(n):
   fact=1
@@ -34,7 +36,7 @@ for i in range(n):
     # for new line
 	print()
 print()
-print()
+
 
 print("Using Recurssion:-")
 
@@ -56,12 +58,9 @@ def pascal_triangle(n,originalength=n):
     print()
 pascal_triangle(n)
 print("\n")
-
-
-
-
+print("-"*80)
 #Q3
-
+print("\n Q3 \n")
 int1, int2 = map(int,input("Enter 2 numbers:- \n").split())
 Quotient = int1 // int2
 Remainder = int1 % int2
@@ -80,7 +79,7 @@ if (Remainder == 0):
     print("Remainder is zero.")
 if (Quotient != 0 and Remainder != 0):
     print("All of the values are non-zero.")
-
+print("\n")
 #part c)
 print("c) \n")
 list = [Quotient + 4, Remainder + 4, Remainder + 5, Quotient + 5, Remainder + 5, Quotient + 6, Remainder + 6]
@@ -89,97 +88,95 @@ result = []
 for i in range(len(list)):
     if list[i] > 4:
         result.append(list[i])
-print(f"The filtered numbers that are greater than 4 are : {result}")
+print(f"The filtered numbers that are greater than 4 are : {result} \n")
 
 #part d)
 print("d) \n")
 setresult = set(result)
-print("Set:",setresult)
-
+print("Set:",setresult) 
+print("\n")
 #part e)
 print("e) \n")
 immutableSet = frozenset(setresult)
 #Frozen Set makes the set immutable.
 print("Immutable set:- ",immutableSet)
-
+print("\n")
 #part f)
 print("f) \n")
 print("Hash value of the max value from the above set:- ", hash(max(immutableSet)))
-print()
-
+print("\n")
+print("-"*80)
 #Q4
-
+print("\n Q4 \n")
 class Student:
-    def init1(self, student,rollno):
+    def __init__(self, student,rollno):
         self.name = student
-        self.roll_no = rollno
+        self.rollno = rollno
 
-    def del1(self):
+    def __del__(self):
         print("Object deleted.")
+
 
 #Creating object
 student1 = Student("Rajat", 21104032)
 print("Object created")
 #printing the assigned values
-print(f"The name of the student it {student1.name} and SID is {student1.roll_no}.")
+print(f"The name of the student it {student1.name} and SID is {student1.rollno}.")
 #deleting object
 del student1
 print("\n")
-
+print("-"*80)
 #Q5
-
+print("\n Q5 \n")
 class Employee:
-    def _init_(self, name, salary):
+    def __init__(self, name, salary):
         self.name = name
         self.salary = salary
+    def print_data(self):
+        print("%s has a salary of %d rupees" % (self.name,self.salary))
 #Creating Employee records 
 employee1 = Employee("Mehak", 40000)
 employee2 = Employee("Ashok", 50000)
 employee3 = Employee("Viren", 60000)
 
+print("The current database is:")                                                                       #Printing the initial values
+for i in [employee1,employee2,employee3]:
+    i.print_data()
+
 #a) Updating salary
 employee1.salary = 70000
 print("a) \n")
-print(f" The updated salary of {employee1.name} is {employee1.salary} ")
+print(f" The updated salary of {employee1.name} is {employee1.salary} \n")
 #b) Deleting a record
 print("b \n")
 print("Record of Viren deleted", end='')
 del employee3
 print("\n")
+print("-"*80)
 
 #Q6
+print("\n Q6 \n")
+#Defining anagram function
+def anagram(word):
+    if len(word)==1:
+        return [word]
+    other_words=anagram(word[1:])
+    char=word[0]
+    list1=[]
+    for combination in other_words:
+        for i in range(len(combination)+1):
+            list1.append(combination[:i]+char+combination[i:])
+    return list1      
 
-#Input a word from the first friend
-word =input("Enter the word: ")
-word=word.lower()
 
-#Input a meaningful word with the exact same letters.
-testword = input("Enter a new meaningful word using the exact same letters to test your friendship: ")
-testword=testword.lower()
-#Defining dictionary
-def count_in_dict(word):
-    count = {}
-    list1 = list(word)
-    
-    n = len(list1)
-    for i in range(n):
-        if list1[i] in count:
-            count[list1[i]] += 1
-        else:
-            count[list1[i]] = 1
-    return count
-#Shopkeeper's input to verify the meaning of the word.
-def userinput():
-    if count_in_dict(word) != count_in_dict(testword):
-        print("The letters aren't same, friendship is fake")
-        return
-    ans = input("Does the word makes sense?(y/Y or n/N )")
+George_word=input("George's word:-").lower()
+Barbie_word=input("Barbie's word:-").lower()
+other_words=anagram(George_word)
+print("Other possible words are:",other_words)
+print("If Barbie's word is present in the list , then their friendship is real. \n")
 
-    if ans == 'y' or ans=='Y':
-        print("The friends pass the friendship test")
-    elif ans == 'n' or ans=='N':
-        print("The word doesn't have a meaning, friendship is fake")
-    else:
-        print("Invalid input,try again with y/Y or n/N ")
-        userinput()
-userinput()
+if Barbie_word in other_words:
+    print("The Friendship is real.")
+else:
+    print("The Friendship is fake.")
+print("-"*80)
